78cfc712157176f31ce7d67511c8c685aa69afa8
dervalinor <73512305+dervalinor@users.noreply.github.com>
2020-12-04 22:42:29 -0500

    Add files via upload

---
+++ Factores.R
@@ -0,0 +1,14 @@
+#the factors are use for category 
+#create of a factor
+#example
+v = c('M', 'm','m', 'g', 's', 'S')
+factor(v) #organize of the vector in category
+
+#Note: Character vector
+character(length = 4) #create 4 space for characters
+as.character(2) #convert in the character
+is.character(a) #see if is a character  
+
+#create a factor
+a = c(2, 4, 1, 3)
+b = c('a', 'b', 'c', 'd')
